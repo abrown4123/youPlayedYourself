fceb357d10816cc07044df5364a44b7a9343e116
Anthony Brown <abrown4123@gmail.com>
2018-09-24 16:51:13 -0500

    Init commit

---
+++ index.js
@@ -0,0 +1,22 @@
+
+let URL = 'http://rocky-ravine-34479.herokuapp.com/anotherone';
+
+function getData(theUrl) {
+  let xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() {
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+      let info = JSON.parse(xmlHttp.responseText);
+      replaceDefaultImage(info.data)
+    }
+  }
+  xmlHttp.open("GET", theUrl, true); // true for asynchronous
+  xmlHttp.send(null);
+}
+
+function replaceDefaultImage(data) {
+  var khaledGif = document.querySelector('#khaled-gif');
+  khaledGif.setAttribute('src', data.image_url);
+  khaledGif.onload = () => document.querySelector('h1').className += ' hide';
+}
+
+getData(URL);
